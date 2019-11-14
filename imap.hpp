ef4088861d60277871b1fe8f32f6b213140a1aa7
andrewlaikh <andrewlaikh1@gmail.com>
2019-11-14 13:01:09 +0000

    Add files via upload

--- imap.hpp
+++ imap.hpp
@@ -10,7 +10,10 @@ namespace IMAP {
 class Message {
 public:
 	//this is the constructor
-	Message(){}
+	//use initializer list for session and uid
+	Message(mailimap* session, uint32_t uid, int msgCount):session(session), uid(uid), msgCount(msgCount){}
+	//
+	//
 	/**
 	 * Get the body of the message. You may chose to either include the headers or not.
 	 */
@@ -22,10 +25,25 @@ public:
 	/**
 	 * Remove this mail from its mailbox
 	 */
+	 //self-defined function to fetch message
+	void fetch_msg(struct mailimap* imap, uint32_t uid);
+	// //self-defined function to get
+	// // char* get_msg_att_msg_content(struct mailimap_msg_att * msg_att, size_t* p_msg_size);
+	// //
+	// // char* get_msg_content(clist* fetch_result, size_t* p_msg_size);
+	//
 	void deleteFromMailbox();
 	//not included, so is there a need for a destructor on the message side?
 
 private:
+	mailimap* session;
+	uint32_t uid;
+	//remember that this must be freed
+	clist* fetch_result_storage;
+	size_t* p_msg_size;
+	//temp variable
+	uint32_t msgCount;
+
 };
 
 class Session {
@@ -35,7 +53,6 @@ public:
 	//function is a wrapper that can wrap and kind of callable element into a copyable object
 	//https://stackoverflow.com/questions/20353210/usage-and-syntax-of-stdfunction
 	Session(std::function<void()> updateUI);
-
 	/**
 	* Get all messages in the INBOX mailbox terminated by a nullptr (like we did in class)
 	*/
@@ -43,18 +60,19 @@ public:
 	/**
 	* connect to the specified server (143 is the standard unencrypte imap port)
 	*/
-	//self-defined function to fetch mesage. probably best left to the message class (review later)
-	void fetch_msg(struct mailimap* imap, uint32_t uid);
+	//self-defined function to fetch message content (should moved into message portion)
+	//self-defined function to get message attribute message content
 
 	//self-defined function to get UID for a specific message
-	static uint32_t get_uid(struct mailimap_msg_att* msg_att); 
+	uint32_t get_uid(struct mailimap_msg_att* msg_att);
+	//get message count
+	int get_msg_count();
 
 	void connect(std::string const& server, size_t port = 143);
 	/**
 	* log in to the server (connect first, then log in)
 	*/
 	void login(std::string const& userid, std::string const& password);
-
 	/**
 	* select a mailbox (only one can be selected at any given time)
 	*
@@ -68,6 +86,10 @@ public:
 
 private:
 	mailimap* session;
+	Message** msgPtr = NULL;
+	uint32_t UID = 36;
+	//temp variable
+	int msgCount;
 	//NOT YET freed the underlying mailbox reference
 	const char* mb;
 };
