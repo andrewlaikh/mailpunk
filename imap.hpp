d4b837c8c9a32d6c754db6a914b2e82c59d04a4a
andrewlaikh <andrewlaikh1@gmail.com>
2019-11-19 15:50:28 +0000

    Add files via upload

--- imap.hpp
+++ imap.hpp
@@ -5,55 +5,48 @@
 #include <string>
 #include <functional>
 #include <cstring>
-//self-included
-#include <vector>
+
 
 namespace IMAP {
-class Message {
-public:
-	//this is the constructor
-	//use initializer list for session and uid
-	Message(mailimap* session, uint32_t uid, int msgCount):session(session), uid(uid), msgCount(msgCount){}
-	//
-	//self-defined function to fetch message
- 	// void fetch_msg();
-	/**
-	 * Get the body of the message. You may chose to either include the headers or not.
-	 */
-	std::string getBody();
-	/**
-	 * Get one of the descriptor fields (subject, from, ...)
-	 */
-	std::string getField(std::string fieldname);
-	/**
-	 * Remove this mail from its mailbox
-	 */
+	class Session;
 
-	// self-defined function to get msg att, worth modifying into string to return
-	std::string get_msg_content(struct mailimap_msg_att * msg_att);
-	//  self-defined function to get msg header
-	std::string get_msg_header(struct mailimap_msg_att * msg_att, std::string fieldname);
-	//
-	// // char* get_msg_content(clist* fetch_result, size_t* p_msg_size);
-	//
-	// char* get_msg_header(struct mailimap_msg_att * msg_att);
+	class Message {
+	public:
+		Message(Session* session, uint32_t uid, std::function<void()> updateUI):sessionPtr(session), uid(uid), updateUI(updateUI){};
+		//
+		std::string getBody();
+		/**
+		 * Get one of the descriptor fields (subject, from, ...)
+		 */
+		std::string getField(std::string fieldname);
+		/**
+		 * Remove this mail from its mailbox
+		 */
+		// self-defined function to get msg att, worth modifying into string to return
+		std::string get_msg_content(struct mailimap_msg_att * msg_att);
+		//  self-defined function to get msg header
+		std::string get_msg_header(struct mailimap_msg_att * msg_att, std::string fieldname);
 
-	void deleteFromMailbox();
-	//not included, so is there a need for a destructor on the message side?
+		//free content/header
+		void freeHelperFunction(struct mailimap_set* mailimapSetSingle, clist* fetchResult, struct mailimap_fetch_type* fetchType);
 
-private:
-	mailimap* session;
-	uint32_t uid;
-	uint32_t msgCount;
-	struct mailimap_flag_list* mailimapFlagList; 
+		void deleteFromMailbox();
 
-};
+		//return empty mailbox to prevent mem leaks.
+		void setMsgEmpty();
+
+		uint32_t uid;
+
+	private:
+		Session* sessionPtr;
+		std::function<void()> updateUI;
+
+	};
 
 class Session {
 public:
 	//constructor for session
 	//http://www.cplusplus.com/reference/functional/function/?kw=function
-	//function is a wrapper that can wrap and kind of callable element into a copyable object
 	//https://stackoverflow.com/questions/20353210/usage-and-syntax-of-stdfunction
 	Session(std::function<void()> updateUI);
 	/**
@@ -63,13 +56,14 @@ public:
 	/**
 	* connect to the specified server (143 is the standard unencrypte imap port)
 	*/
-	//self-defined function to get UID for a specific message
+	//self-defined function
 	uint32_t get_uid(struct mailimap_msg_att* msg_att);
 
-	//get message count
+	//self-defined function
 	int get_msg_count();
 
 	void connect(std::string const& server, size_t port = 143);
+
 	/**
 	* log in to the server (connect first, then log in)
 	*/
@@ -81,17 +75,19 @@ public:
 	*/
 	void selectMailbox(std::string const& mailbox);
 
+	//be careful with the below. SElf-defined function
+	void deleteAllMail();
+
 	~Session();
 
 	//also consider whether destruction is needed, but not imapSession in destructor
+	Message** msgPtr = NULL;
+	std::function<void()> updateUI;
+	int msgCount;
+	mailimap* imapSession = NULL;
 
 private:
-	mailimap* session = NULL;
-	Message** msgPtr = NULL;
-	//temp variable
-	int msgCount;
-	//NOT YET freed the underlying mailbox reference
-	const char* mb;
+	std::string mailboxStore;
 };
 }
 
